a7499752d7be4f9899a917a03b9991198df430df
Eckhart Koeppen <eck@40hz.org>
2018-03-26 14:20:46 +0300

    Move IRQ handler to generic package

--- examples/stm32f0/timer/peripherals.ads
+++ examples/stm32f0/timer/peripherals.ads
@@ -14,7 +14,7 @@ package Peripherals is
       Pin => GPIO.Pin_9, Port => GPIO.Port_A,
       Pull_Resistor => GPIO.Pull_Up, Mode => GPIO.Mode_AF, Alternate_Function => 1);
 
-   package USART is new STM32GD.USART.Peripheral (USART => STM32GD.USART.USART_1, Speed => 115200);
+   package USART is new STM32GD.USART.Peripheral (USART => STM32GD.USART.USART_1, Speed => 115200, IRQ => Ada.Interrupts.Names.USART1);
    package Timer is new STM32GD.Timer.Peripheral (Timer => STM32GD.Timer.Timer_14, IRQ => Ada.Interrupts.Names.TIM14);
 
    procedure Init;
