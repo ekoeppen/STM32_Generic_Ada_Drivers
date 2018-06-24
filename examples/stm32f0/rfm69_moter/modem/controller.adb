6d9d4d13791fa29750b357afacc285c3bded6e2a
Eckhart Koeppen <eck@40hz.org>
2018-06-24 21:11:04 +0300

    Package usage simplifications

--- examples/stm32f0/rfm69_moter/modem/controller.adb
+++ examples/stm32f0/rfm69_moter/modem/controller.adb
@@ -4,7 +4,7 @@ with STM32GD.Board;                 use STM32GD.Board;
 with STM32GD.USART;                 use STM32GD.USART;
 with Ada.Real_Time;                 use Ada.Real_Time;
 with Ada.Synchronous_Task_Control;  use Ada.Synchronous_Task_Control;
-with Peripherals;                   use Peripherals;
+with Peripherals;                   use Peripherals; use Peripherals.Radio;
 with Utils;                         use Utils;
 with Modem;                         use Modem;
 with Packet;                        use Packet;
@@ -25,12 +25,12 @@ package body Controller is
    -----------------------------------------------------------------------------
 
    package Observation_Encoder is new CBOR (
-      Buffer_Size_Type => Radio.Packet_Size_Type,
-      Buffer_Type => Radio.Packet_Type);
+      Buffer_Size_Type => Packet_Size_Type,
+      Buffer_Type => Packet_Type);
    use Observation_Encoder;
 
    procedure Encode_Test_Packet is
-      Position	     : Natural := Test_Data'First;
+      Position	: Packet_Size_Type := Test_Data'First;
    begin
       Encode_Tag (Sensor_Reading_Tag, Test_Data, Position);
       Encode_Array (2, Test_Data, Position);
@@ -39,49 +39,95 @@ package body Controller is
       Encode_Decimal_Fraction (33, -1, Test_Data, Position);
    end Encode_Test_Packet;
 
+   procedure Decode_Value (Packet : in Packet_Type;
+      Position : in out Packet_Size_Type) is
+   begin
+      null;
+   end Decode_Value;
+
+   function Decode_Observation (Packet : in Packet_Type ;
+      Position : in out Packet_Size_Type) return Boolean is
+      Tag : Natural;
+   begin
+      if Decode_Tag (Tag, Packet, Position) then
+         case Tag is
+            when Voltage_Tag     => Output.Write ("/Voltage ");
+            when Temperature_Tag => Output.Write ("/Temperature ");
+            when Humidity_Tag    => Output.Write ("/Humidity ");
+            when others          => null;
+         end case;
+         return Decode_Observation (Packet, Position);
+      else
+         return False;
+      end if;
+   end Decode_Observation;
+
+   procedure Decode_Packet (Packet : in Packet_Type) is
+      Position   : Packet_Size_Type := Packet'First;
+      Tag        : Natural;
+      Count      : Natural;
+      Name_Start : Packet_Size_Type;
+   begin
+      if Decode_Tag (Tag, Packet, Position) and then
+         Tag = Sensor_Reading_Tag and then
+         Decode_Array (Count, Packet, Position) and then
+         Count >= 1 and then
+         Decode_Byte_String (Name_Start, Count, Packet, Position) then
+         while Count > 0 loop
+            Output.Write (Character'Pos ('/'));
+            for I in Name_Start .. Name_Start + Packet_Size_Type (Count) loop
+               Output.Write (Packet (I));
+            end loop;
+            exit when not Decode_Observation (Packet, Position);
+            Count := Count - 1;
+            Output.Write (10);
+         end loop;
+      end if;
+   end Decode_Packet;
+
    procedure Handle_Command (Line : Serial_Data) is
    begin
-      Serial.Output.Write_Line (Line);
+      Output.Write_Line (Line);
       if Line.Length > 1 then
          if Line.Data (1) = Transmit_Cmd then
-            Serial.Output.Write_Line ("Transmitting");
+            Output.Write_Line ("Transmitting");
             Modem.TX.Cancel_Send_Repeatedly;
             Modem.TX.Send (Test_Data);
          elsif Line.Data (1) = Continuous_Cmd then
-            Serial.Output.Write_Line ("Continuously transmitting");
+            Output.Write_Line ("Continuously transmitting");
             Modem.TX.Send (Test_Data, Repeat => True);
          elsif Line.Data (1) = Receive_Cmd then
-            Serial.Output.Write_Line ("Receiving");
+            Output.Write_Line ("Receiving");
             Modem.TX.Cancel_Send_Repeatedly;
          elsif Line.Data (1) = Status_Cmd then
-            Serial.Output.Write_Line ("Status");
-            Radio.Print_Registers;
+            Output.Write_Line ("Status");
+            Print_Registers;
          end if;
       end if;
    end Handle_Command;
 
    task body Controller_Task is
       Command_Line : Serial_Data;
-      Packet       : Radio.Packet_Type;
+      Packet       : Packet_Type;
       Packet_Ready : Boolean;
       Next_Release : Time := Clock;
       Period       : constant Time_Span := Milliseconds (100);
    begin
       Blink.Blink_Parameters.Increase_Blink_Count (Blink.Green);
       Encode_Test_Packet;
-      Serial.Output.Write_Line ("Enter command:");
+      Output.Write_Line ("Enter command:");
       loop
-         if Serial.Input.Is_Ready then
-            Serial.Input.Read_Line (Command_Line);
+         if Input.Is_Ready then
+            Input.Read_Line (Command_Line);
             Handle_Command (Command_Line);
          end if;
          Modem.RX.Get_Data (Packet_Ready, Packet);
          if Packet_Ready then
-            Serial.Output.Write_Line ("Got packet");
+            Output.Write_Line ("Got packet");
             for D of Packet loop
-               Serial.Output.Write (To_Hex_String (D));
+               Output.Write (To_Hex_String (D));
             end loop;
-            Serial.Output.Write_Line ("");
+            Output.Write_Line ("");
          end if;
          Next_Release := Next_Release + Period;
          delay until Next_Release;
