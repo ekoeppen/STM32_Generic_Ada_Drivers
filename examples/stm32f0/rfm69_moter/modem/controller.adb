3189f0d44a8603329bb693f1ad4e20e6c838384d
Eckhart Koeppen <eck@40hz.org>
2018-08-04 10:20:25 +0300

    Restructure controller task

--- examples/stm32f0/rfm69_moter/modem/controller.adb
+++ examples/stm32f0/rfm69_moter/modem/controller.adb
@@ -50,6 +50,10 @@ package body Controller is
 
    procedure Write_To_Response (Data : Byte) is
    begin
+      if Data = 16#10# then
+         Response (Response_Index) := Data;
+         Response_Index := Response_Index + 1;
+      end if;
       Response (Response_Index) := Data;
       Response_Index := Response_Index + 1;
    end Write_To_Response;
@@ -83,52 +87,82 @@ package body Controller is
       Response_Encoder.Encode_Tag (Log_Message_Tag);
       Response_Encoder.Encode_Byte_String (Message);
       End_Response;
-      Output.Write (Response, Response'Length);
+      Output.Write (Response, Response_Index - Response'First);
    end Send_Log_Message;
 
-   procedure Handle_Command (Line : Serial_Data) is
-   begin
-      if Line.Length > 1 then
-         if Line.Data (1) = Transmit_Cmd then
-            Send_Log_Message ("Transmitting");
-            Modem.TX.Cancel_Send_Repeatedly;
-            Modem.TX.Send (Test_Data);
-         elsif Line.Data (1) = Continuous_Cmd then
-            Send_Log_Message ("Continuously transmitting");
-            Modem.TX.Send (Test_Data, Repeat => True);
-         elsif Line.Data (1) = Receive_Cmd then
-            Send_Log_Message ("Receiving");
-            Modem.TX.Cancel_Send_Repeatedly;
-         elsif Line.Data (1) = Status_Cmd then
-            Send_Log_Message ("Status");
-            Print_Registers;
-         end if;
-      end if;
-   end Handle_Command;
-
    task body Controller_Task is
-      Command_Line : Serial_Data;
-      Packet       : Packet_Type;
-      Packet_Ready : Boolean;
-      Next_Release : Time := Clock;
-      Period       : constant Time_Span := Milliseconds (100);
-   begin
-      Blink.Blink_Parameters.Increase_Blink_Count (Blink.Green);
-      Encode_Test_Packet;
-      loop
+      Next_Tick    : Time := Clock;
+      Tick_Period  : constant Time_Span := Milliseconds (100);
+
+      procedure Handle_Command (Line : Serial_Data) is
+      begin
+         if Line.Length > 1 then
+            if Line.Data (1) = Transmit_Cmd then
+               Send_Log_Message ("Transmitting");
+               Modem.TX.Cancel_Send_Repeatedly;
+               Modem.TX.Send (Test_Data);
+            elsif Line.Data (1) = Continuous_Cmd then
+               Send_Log_Message ("Continuously transmitting");
+               Modem.TX.Send (Test_Data, Repeat => True);
+            elsif Line.Data (1) = Receive_Cmd then
+               Send_Log_Message ("Receiving");
+               Modem.TX.Cancel_Send_Repeatedly;
+            elsif Line.Data (1) = Status_Cmd then
+               Send_Log_Message ("Status");
+               Print_Registers;
+            end if;
+         end if;
+      end Handle_Command;
+
+      procedure Handle_Serial_Data is
+         Command_Line : Serial_Data;
+      begin
          if Input.Is_Ready then
             Input.Read_Line (Command_Line);
             Handle_Command (Command_Line);
          end if;
+      end Handle_Serial_Data;
+
+      procedure Handle_Modem_Data is
+         Packet       : Packet_Type;
+         Packet_Ready : Boolean;
+      begin
          Modem.RX.Get_Data (Packet_Ready, Packet);
          if Packet_Ready then
+            Start_Response;
             for I in Packet'Range loop
-               Response (I) := Packet (I);
+               Write_To_Response (Packet (I));
             end loop;
-            Output.Write (Response, Packet'Length);
+            End_Response;
+            Output.Write (Response, Response_Index - Response'First);
          end if;
-         Next_Release := Next_Release + Period;
-         delay until Next_Release;
+      end Handle_Modem_Data;
+
+      procedure Send_Heartbeat is
+         Heartbeat_Period   : constant Time_Span := Seconds (5);
+         Ticks_To_Heartbeat : Integer := Heartbeat_Period / Tick_Period;
+      begin
+         Ticks_to_Heartbeat := Ticks_To_Heartbeat - 1;
+         if Ticks_To_Heartbeat = 0 then
+            Ticks_To_Heartbeat := Heartbeat_Period / Tick_Period;
+            Send_Log_Message ("Ping");
+         end if;
+      end Send_Heartbeat;
+
+      procedure Periodic_Tasks is
+      begin
+         Send_Heartbeat;
+      end Periodic_Tasks;
+
+   begin
+      Blink.Blink_Parameters.Increase_Blink_Count (Blink.Green);
+      Send_Log_Message ("Ready");
+      loop
+         Handle_Serial_Data;
+         Handle_Modem_Data;
+         Periodic_Tasks;
+         Next_Tick := Next_Tick + Tick_Period;
+         delay until Next_Tick;
       end loop;
    end Controller_Task;
 
