cb6e6d50e28d5060f6fb23ea530cad7ae4b64d93
Eckhart Koeppen <eck@40hz.org>
2018-08-04 10:29:17 +0300

    Remove simple char based command interface

--- examples/stm32f0/rfm69_moter/modem/controller.adb
+++ examples/stm32f0/rfm69_moter/modem/controller.adb
@@ -15,13 +15,6 @@ package body Controller is
 
    task Controller_Task with Storage_Size => 512;
 
-   Test_Data      : Radio.Packet_Type;
-
-   Transmit_Cmd   : constant Byte := Character'Pos ('t');
-   Continuous_Cmd : constant Byte := Character'Pos ('c');
-   Receive_Cmd    : constant Byte := Character'Pos ('r');
-   Status_Cmd     : constant Byte := Character'Pos ('s');
-
    subtype Response_Size_Type is Positive range 1 .. 64;
    subtype Response_Type is USART_Data (Response_Size_Type);
    Response : Response_Type;
@@ -70,17 +63,6 @@ package body Controller is
       return B;
    end Read_From_Response;
 
-   procedure Encode_Test_Packet is
-   begin
-      Start_Response;
-      Response_Encoder.Encode_Tag (Sensor_Reading_Tag);
-      Response_Encoder.Encode_Array (2);
-      Response_Encoder.Encode_Byte_String ("Test");
-      Response_Encoder.Encode_Tag (Voltage_Tag);
-      Response_Encoder.Encode_Decimal_Fraction (33, -1);
-      End_Response;
-   end Encode_Test_Packet;
-
    procedure Send_Log_Message (Message : String) is
    begin
       Start_Response;
@@ -96,22 +78,7 @@ package body Controller is
 
       procedure Handle_Command (Line : Serial_Data) is
       begin
-         if Line.Length > 1 then
-            if Line.Data (1) = Transmit_Cmd then
-               Send_Log_Message ("Transmitting");
-               Modem.TX.Cancel_Send_Repeatedly;
-               Modem.TX.Send (Test_Data);
-            elsif Line.Data (1) = Continuous_Cmd then
-               Send_Log_Message ("Continuously transmitting");
-               Modem.TX.Send (Test_Data, Repeat => True);
-            elsif Line.Data (1) = Receive_Cmd then
-               Send_Log_Message ("Receiving");
-               Modem.TX.Cancel_Send_Repeatedly;
-            elsif Line.Data (1) = Status_Cmd then
-               Send_Log_Message ("Status");
-               Print_Registers;
-            end if;
-         end if;
+         null;
       end Handle_Command;
 
       procedure Handle_Serial_Data is
