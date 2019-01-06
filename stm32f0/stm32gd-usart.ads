e02406ada8c923989a77f9191c6cca385f9db8fc
Eckhart Koeppen <eck@40hz.org>
2019-01-06 12:22:32 +0200

    Disable USART3 on F0 MCUs for now

--- stm32f0/stm32gd-usart.ads
+++ stm32f0/stm32gd-usart.ads
@@ -5,7 +5,7 @@ package STM32GD.USART is
 
    USART_1 : STM32_SVD.USART.USART_Peripheral renames STM32_SVD.USART.USART1_Periph;
    USART_2 : STM32_SVD.USART.USART_Peripheral renames STM32_SVD.USART.USART2_Periph;
-   USART_3 : STM32_SVD.USART.USART_Peripheral renames STM32_SVD.USART.USART3_Periph;
+   --  USART_3 : STM32_SVD.USART.USART_Peripheral renames STM32_SVD.USART.USART3_Periph;
 
    type USART_Data is array (Natural range <>) of Byte;
 
