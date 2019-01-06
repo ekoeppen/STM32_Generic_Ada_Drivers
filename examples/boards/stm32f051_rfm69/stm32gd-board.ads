b79c16ece556020b11086bec942e73c09e51c62e
Eckhart Koeppen <eck@40hz.org>
2019-01-06 12:20:00 +0200

    Add RFM69 and Nucleo F072 boards

--- examples/boards/stm32f051_rfm69/stm32gd-board.ads
+++ examples/boards/stm32f051_rfm69/stm32gd-board.ads
@@ -9,7 +9,7 @@ package STM32GD.Board is
    package BUTTON     is new GPIO.Pin (Pin => GPIO.Pin_0, Port => GPIO.Port_A);
    package LED_GREEN  is new GPIO.Pin (Pin => GPIO.Pin_0, Port => GPIO.Port_B, Mode => GPIO.Mode_Out);
    package LED_RED    is new GPIO.Pin (Pin => GPIO.Pin_1, Port => GPIO.Port_B, Mode => GPIO.Mode_Out);
-   package LED_YELLOW is new GPIO.Pin (Pin => GPIO.Pin_2, Port => GPIO.Port_B, Mode => GPIO.Mode_Out);
+   package LED_YELLOW is new GPIO.Pin (Pin => GPIO.Pin_3, Port => GPIO.Port_B, Mode => GPIO.Mode_Out);
    package SCLK       is new GPIO.Pin (Pin => GPIO.Pin_5, Port => GPIO.Port_A, Mode => GPIO.Mode_AF);
    package MISO       is new GPIO.Pin (Pin => GPIO.Pin_6, Port => GPIO.Port_A, Mode => GPIO.Mode_AF);
    package MOSI       is new GPIO.Pin (Pin => GPIO.Pin_7, Port => GPIO.Port_A, Mode => GPIO.Mode_AF);
---
+++ examples/boards/stm32nucleo_f072rb/stm32gd-board.adb
@@ -0,0 +1,20 @@
+with STM32_SVD; use STM32_SVD;
+with STM32_SVD.RCC;
+with STM32_SVD.NVIC;
+with STM32_SVD.GPIO;
+with Ada.Text_IO;
+
+package body STM32GD.Board is
+
+   procedure Init is
+   begin
+      STM32_SVD.RCC.RCC_Periph.AHBENR.IOPAEN := 1;
+      STM32_SVD.RCC.RCC_Periph.AHBENR.IOPBEN := 1;
+      STM32_SVD.RCC.RCC_Periph.AHBENR.IOPCEN := 1;
+      STM32_SVD.RCC.RCC_Periph.APB2ENR.SYSCFGEN := 1;
+      BUTTON.Init;
+      LED_RED.Init;
+      LED_GREEN.Init;
+   end Init;
+
+end STM32GD.Board;
