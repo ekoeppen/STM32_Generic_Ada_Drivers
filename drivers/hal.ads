f49dc8101f6865a55b1555432fb6c625411b3287
Eckhart Koeppen <eck@40hz.org>
2019-01-19 22:42:30 +0200

    Add UART to HAL package

--- drivers/hal.ads
+++ drivers/hal.ads
@@ -1,11 +1,13 @@
 with STM32GD.GPIO.Pin;
 with STM32GD.SPI.Peripheral;
 with STM32GD.I2C.Peripheral;
+with STM32GD.USART.Peripheral;
 
 package HAL is
 
-   generic package Pin renames STM32GD.GPIO.Pin;
-   generic package SPI renames STM32GD.SPI.Peripheral;
-   generic package I2C renames STM32GD.I2C.Peripheral;
+   generic package Pin   renames STM32GD.GPIO.Pin;
+   generic package SPI   renames STM32GD.SPI.Peripheral;
+   generic package I2C   renames STM32GD.I2C.Peripheral;
+   generic package USART renames STM32GD.USART.Peripheral;
 
 end HAL;
