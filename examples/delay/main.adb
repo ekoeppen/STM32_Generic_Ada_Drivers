930cc0f563eeb59c985cff915239cd46eca456ca
Eckhart Koeppen <eck@40hz.org>
2019-01-22 22:00:54 +0200

    Example cleanup

--- examples/delay/main.adb
+++ examples/delay/main.adb
@@ -1,5 +1,3 @@
-with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
-
 with Ada.Real_Time; use Ada.Real_Time;
 
 with STM32GD.GPIO;
@@ -8,20 +6,22 @@ with STM32GD.GPIO.Pin;
 with STM32_SVD.RCC;
 with STM32GD.Board; use STM32GD.Board;
 
+with Drivers.Text_IO;
+
 procedure Main is
 
    Next_Release : Time := Clock;
-   Period       : constant Time_Span := Milliseconds (75);  -- arbitrary
+   Period       : constant Time_Span := Milliseconds (500);
+   package Text_IO is new Drivers.Text_IO (STM32GD.Board.USART);
 
 begin
    Init;
    LED_GREEN.Set;
-   USART.Put_Line ("Starting");
+   Text_IO.Put_Line ("Starting");
    loop
-      USART.Put_Line ("Waiting");
+      Text_IO.Put_Line ("Waiting");
       Next_Release := Next_Release + Period;
       delay until Next_Release;
       LED_GREEN.Toggle;
    end loop;
 end Main;
-
