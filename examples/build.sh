a30f403a226da8020c3546d7c7cf978db6f43523
Eckhart Koeppen <eck@40hz.org>
2021-04-21 19:54:43 +0300

    Add STM32F0 timer example

--- examples/build.sh
+++ examples/build.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 examples="empty delay blink"
+stm32f0_examples="stm32f0/timer"
 boards="nucleo_f072rb nucleo_f103rb nucleo_f303re stm32f411e-disco"
 
 for e in ${examples}; do
 	cd "${ADA_STM32_GD}/examples/${e}"
 	for b in ${boards}; do
 		echo "#### Building ${e} for board ${b}"
-		if ! gprbuild -p  -P "${ADA_STM32_GD}/boards/${b}/board.gpr"; then
+		if ! gprbuild -q -p -P "${ADA_STM32_GD}/boards/${b}/board.gpr"; then
 			echo "**** Compilation of ${e} for ${b} failed."
 			exit 1
 		fi
 	done
+done
+
+
+for e in ${stm32f0_examples}; do
+	cd "${ADA_STM32_GD}/examples/${e}"
+	b="nucleo_f072rb"
+	echo "#### Building ${e} for board ${b}"
+	if ! gprbuild -q -p -P "${ADA_STM32_GD}/boards/${b}/board.gpr"; then
+		echo "**** Compilation of ${e} for ${b} failed."
+		exit 1
+	fi
 done
--- examples/stm32f0/timer/peripherals.ads
+++ examples/stm32f0/timer/peripherals.ads
@@ -1,22 +1,11 @@
-with Ada.Interrupts.Names;
-
-with STM32GD.GPIO;
-with STM32GD.GPIO.Pin;
-with STM32GD.USART;
-with STM32GD.USART.Peripheral;
 with STM32GD.Timer;
 with STM32GD.Timer.Peripheral;
+with STM32_SVD.Interrupts;
 
 package Peripherals is
 
-   package GPIO renames STM32GD.GPIO;
-   package TX is new GPIO.Pin (
-      Pin => GPIO.Pin_9, Port => GPIO.Port_A,
-      Pull_Resistor => GPIO.Pull_Up, Mode => GPIO.Mode_AF, Alternate_Function => 1);
-
-   package USART is new STM32GD.USART.Peripheral (USART => STM32GD.USART.USART_1, Speed => 115200, IRQ => Ada.Interrupts.Names.USART1);
-   package Timer is new STM32GD.Timer.Peripheral (Timer => STM32GD.Timer.Timer_14, IRQ => Ada.Interrupts.Names.TIM14);
-
-   procedure Init;
+   package Timer is new STM32GD.Timer.Peripheral (
+                           Timer => STM32GD.Timer.Timer_14,
+                           IRQ => STM32_SVD.Interrupts.TIM14);
 
 end Peripherals;
