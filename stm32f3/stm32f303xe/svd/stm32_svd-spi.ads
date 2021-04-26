4ff29c3026c4b24280baf22a5b81ea9969375466
Eckhart Koeppen <eck@40hz.org>
2021-04-26 18:57:11 +0300

    Fix STM32F3 SPI

--- stm32f3/stm32f303xe/svd/stm32_svd-spi.ads
+++ stm32f3/stm32f303xe/svd/stm32_svd-spi.ads
@@ -206,15 +206,12 @@ package STM32_SVD.SPI is
    type DR_Register is record
       --  Data register
       DR             : DR_DR_Field := 16#0#;
-      --  unspecified
-      Reserved_8_31 : STM32_SVD.UInt24 := 16#0#;
    end record
-     with Volatile_Full_Access, Size => 32,
+     with Volatile_Full_Access, Size => 8,
           Bit_Order => System.Low_Order_First;
 
    for DR_Register use record
       DR             at 0 range 0 .. 7;
-      Reserved_8_31 at 0 range 8 .. 31;
    end record;
 
    subtype CRCPR_CRCPOLY_Field is STM32_SVD.UInt16;
@@ -372,7 +369,7 @@ package STM32_SVD.SPI is
       CR1     at 16#0# range 0 .. 31;
       CR2     at 16#4# range 0 .. 31;
       SR      at 16#8# range 0 .. 31;
-      DR      at 16#C# range 0 .. 31;
+      DR      at 16#C# range 0 .. 7;
       CRCPR   at 16#10# range 0 .. 31;
       RXCRCR  at 16#14# range 0 .. 31;
       TXCRCR  at 16#18# range 0 .. 31;
