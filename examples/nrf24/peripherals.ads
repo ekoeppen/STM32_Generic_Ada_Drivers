a058ff1dd93571ba7c0cbb0b2e41c72ccda4f4ee
Eckhart Koeppen <eck@40hz.org>
2021-04-26 18:56:27 +0300

    Fix NRF24 example pins

--- examples/nrf24/peripherals.ads
+++ examples/nrf24/peripherals.ads
@@ -12,11 +12,9 @@ with Drivers.NRF24;
 
 package Peripherals is
 
-   package CE     is new Pin (Pin => Pin_1, Port => Port_A, Mode => Mode_Out);
-   package IRQ    is new Pin (Pin => Pin_2, Port => Port_A, Mode => Mode_In);
-   package Timer  is new STM32GD.Timer.Peripheral (
-                              Timer => STM32GD.Timer.Timer_7,
-                              IRQ => STM32_SVD.Interrupts.TIM7);
+   package CE     is new Pin (Pin => Pin_10, Port => Port_B, Mode => Mode_Out);
+   package IRQ    is new Pin (Pin => Pin_4, Port => Port_B, Mode => Mode_In);
+   package Timer  is new STM32GD.Timer.Peripheral;
    package Radio  is new Drivers.NRF24 (
                               SPI => STM32GD.Board.SPI,
                               Chip_Select => STM32GD.Board.CSN,
--- examples/nrf24_zfp/main.adb
+++ examples/nrf24_zfp/main.adb
@@ -1,19 +1,22 @@
 with STM32GD.Board; use STM32GD.Board;
+with STM32GD.GPIO; use STM32GD.GPIO;
+with STM32GD.GPIO.Pin;
 with Drivers.Text_IO;
+with Peripherals;
 
 procedure Main is
 
-   procedure Print_Registers is new Radio.Print_Registers (Put_Line => Text_IO.Put_Line);
+   procedure Print_Registers is new Peripherals.Radio.Print_Registers (Put_Line => Text_IO.Put_Line);
 
    procedure RX_Test is
-      RX_Address        : constant Radio.Address_Type := (16#00#, 16#F0#, 16#F0#, 16#F0#, 16#F0#);
+      RX_Address        : constant Peripherals.Radio.Address_Type := (16#00#, 16#F0#, 16#F0#, 16#F0#, 16#F0#);
    begin
       Text_IO.Put_Line ("Starting RX test");
-      Radio.Set_RX_Address (RX_Address);
-      Radio.RX_Mode;
+      Peripherals.Radio.Set_RX_Address (RX_Address);
+      Peripherals.Radio.RX_Mode;
       loop
          LED.Toggle;
-         if Radio.Wait_For_RX then
+         if Peripherals.Radio.Wait_For_RX then
             Text_IO.Put_Line ("Packet received");
          end if;
          Print_Registers;
@@ -21,20 +24,20 @@ procedure Main is
    end RX_Test;
 
    procedure TX_Test is
-      Broadcast_Address : constant Radio.Address_Type := (16#00#, 16#F0#, 16#F0#, 16#F0#, 16#F0#);
-      TX_Data           : constant Radio.Packet_Type := (16#00#, 16#FF#, 16#00#, 16#00#, 16#00#, 16#00#, 16#00#, 16#FF#, 16#55#);
+      Broadcast_Address : constant Peripherals.Radio.Address_Type := (16#00#, 16#F0#, 16#F0#, 16#F0#, 16#F0#);
+      TX_Data           : constant Peripherals.Radio.Packet_Type := (16#00#, 16#FF#, 16#00#, 16#00#, 16#00#, 16#00#, 16#00#, 16#FF#, 16#55#);
       Line              : String (1 .. 8);
       Len               : Natural;
    begin
       Text_IO.Put_Line ("Starting TX test");
-      Radio.Set_TX_Address (Broadcast_Address);
+      Peripherals.Radio.Set_TX_Address (Broadcast_Address);
       loop
          Text_IO.Put_Line ("Press <enter>");
          Text_IO.Get_Line (Line, Len);
          LED.Set;
-         Radio.TX_Mode;
-         Radio.TX (TX_Data);
-         Radio.Power_Down;
+         Peripherals.Radio.TX_Mode;
+         Peripherals.Radio.TX (TX_Data);
+         Peripherals.Radio.Power_Down;
          Text_IO.Put_Line ("Packet sent");
          Print_Registers;
          LED.Clear;
@@ -43,8 +46,8 @@ procedure Main is
 
 begin
    Init;
-   Radio.Init;
-   Radio.Set_Channel (70);
+   Peripherals.Init;
+   Peripherals.Radio.Set_Channel (70);
    Print_Registers;
    TX_Test;
 end Main;
