7ca90af674f619902c147d85e8612965b0061c17
Eckhart Koeppen <eck@40hz.org>
2021-12-04 07:40:40 +0200

    Updated STM32F0 boards

--- boards/stm32f030x8/stm32gd-board.ads
+++ boards/stm32f030x8/stm32gd-board.ads
@@ -1,13 +1,21 @@
-with STM32GD.GPIO; use STM32GD.GPIO;
+with STM32_SVD.GPIO; use STM32_SVD.GPIO;
+with STM32_SVD.SPI;
+with STM32_SVD.TIM;
+with STM32_SVD.Interrupts;
+with STM32_SVD.USART;
 
-with STM32GD.GPIO.Pin;
+with STM32GD.GPIO;
+with STM32GD.USART;
+with STM32GD.SPI;
+with STM32GD.GPIO;
+with STM32GD.RTC;
+with STM32GD.Clock;
+with STM32GD.Clock.Tree;
 
 package STM32GD.Board is
 
-   package GPIO renames STM32GD.GPIO;
-
-   package BUTTON is new GPIO.Pin (Pin => GPIO.Pin_0, Port => GPIO.Port_A);
-   package LED    is new GPIO.Pin (Pin => GPIO.Pin_6, Port => GPIO.Port_F, Mode => GPIO.Mode_Out);
+   package BUTTON    is new STM32GD.GPIO (Pin => 0, Port => GPIOA_Periph);
+   package LED       is new STM32GD.GPIO (Pin => 1, Port => GPIOA_Periph, Output => True);
 
    procedure Init;
 
